36a33ad79fa512c3a4251e6ad711f4c399c63970
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2026-07-09 21:58:43 +0530

    task: add start script

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     allowedHeaders: ["Content-Type", "X-UserAPI-Key"],
     methods: ["GET", "POST", "OPTIONS"],
   }),
